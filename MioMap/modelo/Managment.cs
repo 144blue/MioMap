efd684afd1f22563c2224b422dab3ef44406d711
144blue <aletru321@gmail.com>
2019-09-29 19:45:36 -0500

    ready to work with ordened stops.cvs

--- MioMap/modelo/Managment.cs
+++ MioMap/modelo/Managment.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +8,16 @@ namespace modelo
 {
     class Managment
     {
-// aqui hay que pasar todo lo de leer y eso para hacerlo con buenas practicas 
-// para eso de cargar esa cantidad de datos estoy pensando en filtrar los datos por partes 
-// ejemplo coger de los mil y pico unos 100 y de ahi aplicarle los metodos para reducir y 
-// que queden los menos posibles y asi con todo, por ahora voy a guardar la porcion en un arreglo de 100 y trabajar
-// sobre eso y ya luego meter eso a una hash.
+        // aqui hay que pasar todo lo de leer y eso para hacerlo con buenas practicas 
+        // para eso de cargar esa cantidad de datos estoy pensando en filtrar los datos por partes 
+        // ejemplo coger de los mil y pico unos 100 y de ahi aplicarle los metodos para reducir y 
+        // que queden los menos posibles y asi con todo, por ahora voy a guardar la porcion en un arreglo de 100 y trabajar
+        // sobre eso y ya luego meter eso a una hash.
 
+        private Hashtable StopsHash;
         
+
+            
 
         
 
